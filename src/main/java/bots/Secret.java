3486ea1b21fb62e9167805f87a12b1a3308e7472
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2021-07-29 23:23:46 -0400

    TOKEN

--- src/main/java/bots/Secret.java
+++ src/main/java/bots/Secret.java
@@ -1,5 +1,5 @@
 package bots;
 
 public class Secret {
-    public static String TOKEN = "ODcwNTA0NzgxNDU2NzQ4NTY0.YQNuwQ.MEMBEmqw_DRwrFYWVKmhosOv8Jk";
+    public static String TOKEN = "never gonna give you up";
 }
